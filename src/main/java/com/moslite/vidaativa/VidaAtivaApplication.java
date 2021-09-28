086831ae43b58c328db1c6e5014c3638ca8b8d8b
Murilo Souza <moslite@hotmail.com>
2021-09-28 13:40:11 -0300

    Project created

---
+++ src/main/java/com/moslite/vidaativa/VidaAtivaApplication.java
@@ -0,0 +1,13 @@
+package com.moslite.vidaativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidaAtivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidaAtivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moslite/vidaativa/VidaAtivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moslite.vidaativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaAtivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
